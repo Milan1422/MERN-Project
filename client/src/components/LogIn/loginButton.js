85ddce8bafb888db4b803dbd84ca14dbfeb16911
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-05-11 20:40:07 -0400

    added bootstrap testing merging security

---
+++ client/src/components/LogIn/loginButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginBtn () {
+    return(
+        <button className='btn btn-danger rounded-pill'>
+            Log In
+        </button>
+    )
+}
+
+export default LoginBtn;
