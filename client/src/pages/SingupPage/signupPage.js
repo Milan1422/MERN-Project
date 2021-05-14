dfccf1ce02e7a5b000b051e75f3d4d271bbd2f05
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-13 23:44:19 -0400

    added data for seeds and changes made to signup page

--- client/src/pages/SingupPage/signupPage.js
+++ client/src/pages/SingupPage/signupPage.js
@@ -4,7 +4,7 @@ function SignupPage() {
   return (
     <div>
       <h1>Create Account</h1>
-      <form>
+      <form id ="signup-form">
         <div className="form-group">
           <label>Email address</label>
           <input
@@ -15,31 +15,81 @@ function SignupPage() {
           />
         </div>
         <div className="form-group">
+          <label>User Name</label>
+          <input
+            type="username"
+            className="form-control"
+            id="signUp-username"
+          />
+        </div>
+
+        <div className="form-group">
           <label>Password</label>
           <input
             type="password"
             className="form-control"
-            id="signup-password"
+            id="signUp-password"
           />
         </div>
         <div className="form-group">
           <label>Skill (Language)</label>
-          <input type="password" className="form-control" id="signup-skill" />
+          <input type="string" className="form-control" id="signup-skill" />
         </div>
         <div className="form-group">
           <label>Location</label>
           <input
-            type="password"
+            type="string"
             className="form-control"
             id="signup-location"
           />
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary rounded-pill">
           Sign Up
         </button>
       </form>
     </div>
   );
+ 
+ 
+  async function signupFormHandler(event) {
+    event.preventDefault();
+    
+
+    // getting data from the form
+    const username = document.querySelector('#signUp-username').value;
+    console.log(username);
+    const password = document.querySelector('#signUp-password').value;
+    console.log(password);
+    if (username && password) {
+      console.log('hello');
+        const response = await fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify({
+            username,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        }); 
+    // check the response status
+    if (response.ok) {
+        console.log('success');
+
+        // loginHandler();
+        document.location.replace('/profile');
+
+      } else {
+        alert(response.statusText);
+      }
+    }
 }
+
+
+
+
+
+
+}
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler) 
 
 export default SignupPage;
