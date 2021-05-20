1588ea4623b4ec6376907429f65e86e6a8878254
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-20 13:07:36 -0400

    changes to formsignup

--- client/src/pages/SignupPage/FormSignup.js
+++ client/src/pages/SignupPage/FormSignup.js
@@ -1,97 +1,199 @@
-import React from "react";
-import useForm from "../../pages/SignupPage/useForm";
+import React, { Component } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardTitle,
+  CardSubtitle,
+  CardBody,
+  Alert,
+  Spinner,
+} from "reactstrap";
+import React, { Component } from "react";
+import { connect } from "react-redux"; // API to connect component state to redux store
+import PropTypes from "prop-types";
+import { buttonClicked, isLoading } from "../../actions/frontEndActions";
+import { Link } from "react-router-dom";
+import { register } from "../../actions/authActions";
 
-const FormSignup = ({ submitForm }) => {
-  const { handleChange, values, handleSubmit } = useForm();
+class Register extends Component {
+  state = {
+    email: "",
+    username: "",
+    password: "",
+    skills: "",
+    location: "",
+    msg: "",
+  };
 
-  return (
-    <div className="form-content-right">
-      <form className="form" onSubmit={handleSubmit}>
-        <h1> Sign Up!</h1>
-        <div className="form-inputs">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="form-input"
-            placeholder="Enter your email"
-            value={values.email}
-            onChange={handleChange}
-          />
-        </div>
+  static propTypes = {
+    buttonClicked: PropTypes.func.isRequired,
+    button: PropTypes.bool,
+    register: PropTypes.func.isRequired,
+    status: PropTypes.object.isRequired,
+    loading: PropTypes.bool,
+  };
 
-        <div className="form-inputs">
-          <label htmlFor="username" className="form-label">
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            className="form-input"
-            placeholder="Create a Username"
-            value={values.username}
-            onChange={handleChange}
-          />
-        </div>
+  // Removes sign in and register buttons from homepage
+  // upon mounting of Register component
+  componentDidMount() {
+    this.props.buttonClicked();
+  }
 
-        <div className="form-inputs">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="form-input"
-            placeholder="Create a unique Password"
-            value={values.password}
-            onChange={handleChange}
-          />
-        </div>
+  componentDidUpdate(prevProps) {
+    const status = this.props.status;
 
-        <div className="form-inputs">
-          <label htmlFor="Skill" className="form-label">
-            Skill
-          </label>
-          <input
-            type="text"
-            name="skill"
-            id="skill"
-            className="form-input"
-            placeholder="List your coding skills!"
-            value={values.skill}
-            onChange={handleChange}
-          />
-        </div>
+    // Changes status message if it is different from previous message
+    if (status !== prevProps.status) {
+      if (status.id === "REGISTER_FAIL") {
+        this.setState({ msg: status.statusMsg });
+      } else {
+        this.setState({ msg: this.props.status.statusMsg });
+      }
+    }
 
-        <div className="form-inputs">
-          <label htmlFor="Skill" className="form-label">
-            Location
-          </label>
-          <input
-            type="text"
-            name="location"
-            id="location"
-            className="form-input"
-            placeholder="List your coding skills!"
-            value={values.location}
-            onChange={handleChange}
-          />
-        </div>
+    // Redirects to Log In screen after a delay of 2secs if successfully registered
+    if (status.id === "REGISTER_SUCCESS") {
+      setTimeout(() => {
+        this.props.history.push("/login");
+      }, 2000);
+    }
+  }
 
-        <button className="form-input-btn btn-primary" type="submit">
-          Sign Up
-        </button>
-        <span className="form-input-login">
-          Already have an account? Login <a href="#">here</a>
-        </span>
-      </form>
-    </div>
-  );
-};
+  // Sets the value of the input fields to the state items of the same name
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-export default FormSignup;
+  // Calls action to register user
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, password } = this.state;
+
+    const user = { name, email, password };
+    this.props.isLoading();
+    this.props.register(user);
+  };
+
+  render() {
+    let className = "divStyle";
+
+    // If HTTP 400 error, render alert with red color, else if
+    // it is 200 OK, render alert in green
+    let alert;
+    if (this.state.msg && this.props.status.respCode >= 400) {
+      alert = <Alert color="danger">{this.state.msg}</Alert>;
+    } else if (this.state.msg && this.props.status.respCode === 200) {
+      alert = (
+        <Alert color="success">
+          {this.state.msg} <br /> Redirecting to Log In screen
+        </Alert>
+      );
+    }
+
+    if (!this.props.button) {
+      className = "formStyle";
+    }
+    return (
+      <div className={className}>
+        <Card>
+          <CardBody>
+            <CardTitle>
+              <h2>
+                <strong>Sign Up</strong>
+              </h2>
+            </CardTitle>
+            <CardSubtitle className="text-muted">
+              Already have an account?
+              <Link to="/login"> Log In. </Link>
+            </CardSubtitle>
+            <br />
+            {alert}
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup className="text-center">
+                <Label for="username">Name</Label>
+                <Input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Enter your name"
+                  className="mb-3"
+                  size="lg"
+                  onChange={this.onChange}
+                />
+
+                <Label for="email">E-mail</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="you@youremail.com"
+                  className="mb-3"
+                  size="lg"
+                  onChange={this.onChange}
+                />
+
+                <Label for="password">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter your Password"
+                  className="mb-3"
+                  size="lg"
+                  onChange={this.onChange}
+                />
+
+                <Label for="skill">Password</Label>
+                <Input
+                  type="text"
+                  name="skill"
+                  id="skill"
+                  placeholder="Enter your Password"
+                  className="mb-3"
+                  size="lg"
+                  onChange={this.onChange}
+                />
+
+                <Label for="location">Password</Label>
+                <Input
+                  type="text"
+                  name="location"
+                  id="location"
+                  placeholder="Enter your Password"
+                  className="mb-3"
+                  size="lg"
+                  onChange={this.onChange}
+                />
+                <Button color="dark" className="mt-5" size="lg" block>
+                  {this.props.loading ? (
+                    <span>
+                      Registering.. <Spinner size="sm" color="light" />
+                    </span>
+                  ) : (
+                    <span>Sign Up</span>
+                  )}
+                </Button>
+              </FormGroup>
+            </Form>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  //Maps state to redux store as props
+  button: state.ui.button,
+  status: state.status,
+  loading: state.ui.loading,
+});
+
+export default connect(mapStateToProps, { register, isLoading, buttonClicked })(
+  Register
+);
