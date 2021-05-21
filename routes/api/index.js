06f43d654525e256429245a2beb5d74592fe2763
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-05-21 15:55:47 -0400

    separated meets from users controller

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const meetRoutes = require("./meets");
 
 // User routes
 router.use("/users", userRoutes);
+router.use("/meets", meetRoutes);
 
 module.exports = router;
