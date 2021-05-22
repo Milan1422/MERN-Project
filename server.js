e40f5441927def7e3b88e43574ecdb48a1348860
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-21 22:29:11 -0400

    fixed syntax with connectiong to mongo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/userlist", {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
