7a46d3083b4548980d952c97bfc9395afa0f5529
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-05-25 19:55:37 -0400

    jack

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// react components
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// local components
+import "./App.css";
+import Landing from "./pages/Landing/landing";
+import SignupPage from "./pages/SignupPage/signupPage";
+import LoginPage from "./pages/LoginPage/loginpage";
+import Profile from "./pages/Profile/profilePage";
+import Meets from "./pages/Meets/Meets";
+import singleMeet from "./pages/Meets/singleMeet";
+import { StoreProvider } from "./utils/GlobalState";
+import CreateNewMeet from "./pages/Meets/createMeet";
+import JoinedList from "./pages/Meets/joinedMeets";
+import history from "./utils/history"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div>
+        <StoreProvider>
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/login" exact component={LoginPage} />
+            <Route path="/signup" exact component={SignupPage} />
+            <Route path="/profile" exact component={Profile} />
+            <Route path="/meets" exact component={Meets} />
+            <Route path="/meets/:id" exact component={singleMeet} />
+            <Route path="/createmeet" exact component={CreateNewMeet} />
+            <Route path="/joinedmeets" exact component={JoinedList} />
+          </Switch>
+        </StoreProvider>
+      </div>
+    </Router>
   );
 }
 
